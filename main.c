f76d7b958d6bb3f5ebcafa1f292ef831dcc6bdb8
TheKingsident <kingsident@gmail.com>
2024-02-24 21:59:17 +0100

    Added new files

--- main.c
+++ main.c
@@ -3,24 +3,52 @@
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    }
 
     SDL_Window *window = SDL_CreateWindow("My SDL2 Window",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
                                           800, 600, SDL_WINDOW_SHOWN);
+    if (!window)
+    {
+        fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
     SDL_Surface *mapSurface = IMG_Load("level.png");
     if (!mapSurface)
     {
         fprintf(stderr, "Error loading map image: %s\n", IMG_GetError());
-        return (1);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
     }
 
     SDL_Texture *mapTexture = SDL_CreateTextureFromSurface(renderer, mapSurface);
     SDL_FreeSurface(mapSurface);
+    if (!mapTexture)
+    {
+        fprintf(stderr, "Error creating map texture: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, mapTexture, NULL, NULL);
@@ -45,5 +73,5 @@ int main(int argc, char *argv[])
     SDL_DestroyWindow(window);
     SDL_Quit();
 
-    return (0);
+    return 0;
 }
